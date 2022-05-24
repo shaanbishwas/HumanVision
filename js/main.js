49d4f909c1f53689d6ee06d9e7444c406b489df8
shaan_bishwas <shaanbiswas8@gmail.com>
2022-05-24 16:03:20 +0530

    Separated JS from HTML

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var navLinks = document.getElementById("navLinks");
+
+        function showMenu(){
+            navLinks.style.right = "0";
+        }
+        function hideMenu(){
+            navLinks.style.right = "-200px";
+        }
+
+
+        document.querySelector(".compare #compare-ip").addEventListener(
+        "input", function(e){
+        document.querySelector(".compare .before .after").style.width = (+e.target.value) + "px";
+        });
+
+        document.querySelector(".compare #compare-ip").addEventListener(
+        "change", function(e){
+        document.querySelector(".compare .before .after").style.width = (+e.target.value) + "px";
+});
